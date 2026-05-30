395a7bdf540e41d9c3e7edd6d4d51485050a52b4
hamza <160620205+hamza40devloper@users.noreply.github.com>
2026-05-30 03:01:44 +0100

    Update Main.java

--- src/main/java/com/arabblock/sync/Main.java
+++ src/main/java/com/arabblock/sync/Main.java
@@ -1,10 +1,13 @@
 package com.arabblock.sync;
+
+import com.arabblock.sync.commands.LoginCommand;
+import com.arabblock.sync.commands.RegisterCommand;
+import com.arabblock.sync.listeners.PlayerChat;
 import com.arabblock.sync.listeners.PlayerJoin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
-    // تعريف المديرين (Managers)
     private static Main instance;
     private DatabaseManager dbManager;
     private WorldManager worldManager;
@@ -17,7 +20,7 @@ public class Main extends JavaPlugin {
         getLogger().info("====================================");
         getLogger().info("جاري تشغيل نظام ArabBlock Sync...");
         
-        // 1. تشغيل قاعدة البيانات (حفظ بيانات اللاعبين)
+        // 1. تشغيل قاعدة البيانات
         dbManager = new DatabaseManager();
         dbManager.setup();
 
@@ -25,17 +28,20 @@ public class Main extends JavaPlugin {
         worldManager = new WorldManager();
         worldManager.loadLoginWorld();
 
-        // 3. تشغيل نظام الحماية والأكواد العشوائية
+        // 3. تشغيل نظام الحماية
         authManager = new AuthManager();
 
-        // 4. تشغيل خادم محلي صغير (HTTP Server) لانتظار أوامر البوت مثل /setup
+        // 4. تشغيل خادم الاتصال لانتظار البوت
         networkManager = new NetworkManager();
         networkManager.startServer();
 
-        // تسجيل الأحداث (Listeners)
+        // 5. تسجيل الأحداث (Listeners)
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
-        // getServer().getPluginManager().registerEvents(new PlayerChat(), this);
-        // getServer().getPluginManager().registerEvents(new PlayerDeath(), this);
+        getServer().getPluginManager().registerEvents(new PlayerChat(), this);
+
+        // 6. تسجيل الأوامر (Commands)
+        getCommand("register").setExecutor(new RegisterCommand());
+        getCommand("login").setExecutor(new LoginCommand());
 
         getLogger().info("تم التشغيل بنجاح! جاهز للربط مع ديسكورد.");
         getLogger().info("====================================");
@@ -43,12 +49,10 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        // إغلاق الاتصال بقاعدة البيانات والشبكة عند إطفاء السيرفر
         if (networkManager != null) networkManager.stopServer();
         getLogger().info("تم إيقاف نظام ArabBlock Sync.");
     }
 
-    // دوال لجلب المديرين من الكلاسات الأخرى
     public static Main getInstance() { return instance; }
     public AuthManager getAuthManager() { return authManager; }
     public DatabaseManager getDbManager() { return dbManager; }
