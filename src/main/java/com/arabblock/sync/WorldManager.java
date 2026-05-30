5d996a3809a2ae74cb5860d22dfc325f434752e9
hamza <160620205+hamza40devloper@users.noreply.github.com>
2026-05-30 11:43:19 +0100

    Create WorldManager.java

---
+++ src/main/java/com/arabblock/sync/WorldManager.java
@@ -0,0 +1,11 @@
+package com.arabblock.sync;
+
+import org.bukkit.Bukkit;
+
+public class WorldManager {
+
+    public void loadLoginWorld() {
+        Bukkit.getLogger().info("جاري إعداد وتحميل عالم تسجيل الدخول...");
+        // يمكنك إضافة كود مخصص هنا لاحقاً لإنشاء العالم أو تحميل إحداثياته
+    }
+}
